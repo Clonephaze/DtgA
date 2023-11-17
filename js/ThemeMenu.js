dd28a873840a76a98c2f3f76df087192f89a9260
Clonephaze <jackdsmith2327@gmail.com>
2023-11-17 01:01:12 -0800

    Update ThemeMenu.js

--- js/ThemeMenu.js
+++ js/ThemeMenu.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    var colorValue = localStorage.getItem('DtgA-primary') || '133, 255, 225';
+    document.documentElement.style.setProperty('--DtgA-primary', colorValue);
     var colorPicker = new iro.ColorPicker("#ThemeWheel", {
         width: 200,
-        color: "rgb(133, 255, 225)",
+        color: "rgb(" + colorValue + ")",
         layout: [
             {
                 component: iro.ui.Wheel,
@@ -12,19 +14,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     colorPicker.on("color:change", function (color) {
         var rgb = color.rgb;
-        document.documentElement.style.setProperty('--DtgA-primary', rgb.r + ',' + rgb.g + ',' + rgb.b);
+        var colorValue = rgb.r + ',' + rgb.g + ',' + rgb.b;
+        document.documentElement.style.setProperty('--DtgA-primary', colorValue);
+        localStorage.setItem('DtgA-primary', colorValue);
     });
-});
+ }); 
+ 
+    
 
 function toggleThemeColors() {
     const themeDtgA = document.getElementById('ThemeDtgA')
     const themeBlue = document.getElementById('ThemeBlue')
     const themeRed = document.getElementById('ThemeRed')
     const themeGreen = document.getElementById('ThemeGreen')
-    const themeYellow = document.getElementById('ThemeYellow')
-    const themePurple = document.getElementById('ThemePurple')
-    const themeOrange = document.getElementById('ThemeOrange')
-    const themeIndigo = document.getElementById('ThemeIndigo')
 
     themeDtgA.addEventListener('click', function () {
         document.documentElement.style.setProperty('--DtgA-primary', '133, 255, 225');
@@ -40,22 +42,6 @@ function toggleThemeColors() {
 
     themeGreen.addEventListener('click', function () {
         document.documentElement.style.setProperty('--DtgA-primary', '0, 255, 0');
-    })
-
-    themeYellow.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--DtgA-primary', '255, 255, 0');
-    })
-
-    themePurple.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--DtgA-primary', '255, 0, 255');
-    })
-
-    themeOrange.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--DtgA-primary', '255, 128, 0');
-    })
-
-    themeIndigo.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--DtgA-primary', '75, 0, 130');
     })
 }
 
