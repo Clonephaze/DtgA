32f2c0503e0bdf2b1d0c2aaa39d92dcd12ba3c5f
Clonephaze <jackdsmith2327@gmail.com>
2023-11-17 01:52:05 -0800

    Update ThemeMenu.js

--- js/ThemeMenu.js
+++ js/ThemeMenu.js
@@ -30,18 +30,26 @@ function toggleThemeColors() {
 
     themeDtgA.addEventListener('click', function () {
         document.documentElement.style.setProperty('--DtgA-primary', '133, 255, 225');
+        var colorValue = '133, 255, 225';
+        localStorage.setItem('DtgA-primary', colorValue);
     })
 
     themeBlue.addEventListener('click', function () {
         document.documentElement.style.setProperty('--DtgA-primary', '0, 0, 255');
+        var colorValue = '0, 0, 255';
+        localStorage.setItem('DtgA-primary', colorValue);
     })
 
     themeRed.addEventListener('click', function () {
         document.documentElement.style.setProperty('--DtgA-primary', '255, 0, 0');
+        var colorValue = '255, 0, 0';
+        localStorage.setItem('DtgA-primary', colorValue);
     })
 
     themeGreen.addEventListener('click', function () {
         document.documentElement.style.setProperty('--DtgA-primary', '0, 255, 0');
+        var colorValue = '0, 255, 0';
+        localStorage.setItem('DtgA-primary', colorValue);
     })
 }
 
