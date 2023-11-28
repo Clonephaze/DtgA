3a68c41be4e342ca775333504f44942b517bfbf2
Clonephaze <jackdsmith2327@gmail.com>
2023-11-28 14:29:50 -0800

    Fixed js and added another weapon

--- js/CardGeneration.js
+++ js/CardGeneration.js
@@ -17,13 +17,17 @@ function generateCards(jsonFileName, containerId, sectionName) {
         let carousel = document.createElement('div');
         carousel.className = 'carousel slide';
         carousel.id = `carousel-${sectionName}-${index}`;
+
+        let carouselInstance = new bootstrap.Carousel(carousel);
         
         let carouselInner = document.createElement('div');
         carouselInner.className = 'carousel-inner';
         
+        let slideIndex = 0;
         item.imageSrc.forEach((image, imageIndex) => {
           let carouselItem = document.createElement('div');
           carouselItem.className = imageIndex === 0 ? 'carousel-item active' : 'carousel-item';
+          carouselItem.dataset.slideIndex = slideIndex;
           
           let img = document.createElement('img');
           img.src = image;
@@ -33,6 +37,8 @@ function generateCards(jsonFileName, containerId, sectionName) {
           
           carouselItem.appendChild(img);
           carouselInner.appendChild(carouselItem);
+
+          slideIndex++;
         });
         
         carousel.appendChild(carouselInner);
@@ -54,6 +60,8 @@ function generateCards(jsonFileName, containerId, sectionName) {
           
           carousel.appendChild(prevControl);
           carousel.appendChild(nextControl);
+
+          
         }
         
         card.appendChild(carousel);
@@ -70,44 +78,109 @@ function generateCards(jsonFileName, containerId, sectionName) {
         if (Array.isArray(item.description)) {
           let genDesc = document.createElement('div');
           genDesc.className = 'item-card-description';
-          genDesc.innerHTML = data[0].genDesc;
-
+          genDesc.innerHTML = item.genDesc;
           cardBody.appendChild(genDesc);
+         
+          let container = document.createElement('div');
+          container.className = 'container item-card-array-container';
+          container.id = `container-${sectionName}-${index}`;
+         
+          let openAllButton = document.createElement('button');
+          openAllButton.textContent = '[Open All]';
+          openAllButton.className = 'btn open-all-btn';
+          openAllButton.id = `open-all-${sectionName}-${index}`;
+          container.appendChild(openAllButton);
+         
+          let accordion = document.createElement('div');
+          accordion.className = 'accordion';
+          accordion.id = `accordion-${sectionName}-${index}`;
+          container.appendChild(accordion); // Append accordion to container
+         
+          openAllButton.addEventListener('click', function() {
+            let collapsibleElements = document.querySelectorAll(`#accordion-${sectionName}-${index} .collapse`);
+            let allOpen = Array.from(collapsibleElements).every(element => element.classList.contains('show'));
+          
+            if (allOpen) {
+                // If all menus are open, close all menus
+                collapsibleElements.forEach(function(element) {
+                    let bsCollapse = bootstrap.Collapse.getInstance(element);
+                    if (bsCollapse === null) {
+                        bsCollapse = new bootstrap.Collapse(element, { toggle: false });
+                    }
+                    bsCollapse.hide();
+                });
+                openAllButton.textContent = 'Open All';
+            } else {
+                // If not all menus are open, open all menus
+                collapsibleElements.forEach(function(element) {
+                    let bsCollapse = bootstrap.Collapse.getInstance(element);
+                    if (bsCollapse === null) {
+                        bsCollapse = new bootstrap.Collapse(element, { toggle: false });
+                    }
+                    bsCollapse.hide();
+                });
+                openAllButton.textContent = 'Close All';
 
+                setTimeout(function() {
+                  collapsibleElements.forEach(function(element) {
+                      let bsCollapse = bootstrap.Collapse.getInstance(element);
+                      if (bsCollapse === null) {
+                         bsCollapse = new bootstrap.Collapse(element, { toggle: false });
+                      }
+                      bsCollapse.show();
+                  });
+                  openAllButton.textContent = 'Close All';
+                }, 375); // 1000 milliseconds = 1 second
+            }
+         });
+           
+         
+          cardBody.appendChild(container); // Append container to cardBody
+
+          let slideIndex = 0;
           item.description.forEach((desc) => {
-           let idTitle = desc.title.replace(/ /g, "-").replace(/'/g, "");
-           let arrayItem = document.createElement('div');
-           arrayItem.className = 'item-card-array-title btn card-array-btn d-flex flex-column';
-           arrayItem.setAttribute('data-bs-toggle', 'collapse');
-                       
-           let arrayItemContent = document.createElement('div');
-           arrayItemContent.className = 'content item-card-array-content collapse';
-           arrayItemContent.innerHTML = desc.content;
-         
-           arrayItemContent.id = `item-card-array-${idTitle}`;
-           arrayItem.innerHTML = '<span>' + desc.title + '&nbsp;<i id="icon-' + idTitle + '" class="las la-caret-down"></i></span>';
-           arrayItem.setAttribute('href', `#${arrayItemContent.id}`);
-         
-           cardBody.appendChild(arrayItem);
-           cardBody.appendChild(arrayItemContent);
-
-           let iconElement = arrayItem.querySelector('i')
-           let parent = iconElement.parentElement;
-           let grandparent = parent.parentElement;
-
-           let observer = new MutationObserver(function(mutations) {
+          let idTitle = desc.title.replace(/ /g, "-").replace(/'/g, "");
+          let arrayItem = document.createElement('div');
+          arrayItem.className = 'item-card-array-title btn card-array-btn d-flex flex-column';
+          arrayItem.setAttribute('data-bs-toggle', 'collapse');
+          arrayItem.dataset.slideIndex = slideIndex;
+          
+          let arrayItemContent = document.createElement('div');
+          arrayItemContent.className = 'content item-card-array-content collapse';
+          arrayItemContent.innerHTML = desc.content;
+          arrayItemContent.setAttribute('data-bs-parent', `#accordion-${sectionName}-${index}`);
+          
+          arrayItem.addEventListener('click', function() {
+            let slideIndex = this.dataset.slideIndex; 
+            carouselInstance.to(slideIndex);
+          });
+          
+          arrayItemContent.id = `item-card-array-${idTitle}`;
+          arrayItem.innerHTML = '<span>' + desc.title + '&nbsp;<i id="icon-' + idTitle + '" class="las la-caret-down"></i></span>';
+          arrayItem.setAttribute('href', `#${arrayItemContent.id}`);
+          
+          accordion.appendChild(arrayItem);
+          accordion.appendChild(arrayItemContent);
+          
+          let iconElement = arrayItem.querySelector('i')
+          let parent = iconElement.parentElement;
+          let grandparent = parent.parentElement;
+          
+          let observer = new MutationObserver(function(mutations) {
             mutations.forEach(function(mutation) {
               if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
                 if (grandparent.classList.contains('collapsed')) {
                   iconElement.style.transform = 'rotate(0deg)';
-                 } else {
+                } else {
                   iconElement.style.transform = 'rotate(180deg)';
-                 }
+                }
               }
             });
-           });
-           
-           observer.observe(grandparent, { attributes: true });
+          });
+          
+          observer.observe(grandparent, { attributes: true });
+          
+          slideIndex++;
           });
 
          } else {
